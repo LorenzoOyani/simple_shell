c9e1924d07155381595253a6cad382b3ab61f144
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-12-24 00:54:58 +0100

    new commit

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *read_line();
+char **parse_line(char *line);
+char *get_path(char *filename);
+int run_command(char **args, char **argv);
+
+#endif /* define MAIN_H */
